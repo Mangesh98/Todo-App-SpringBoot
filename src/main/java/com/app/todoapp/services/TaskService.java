2602029240a82b2838790a4c88d49b4f3cfdef93
Mangesh98 <mangeshraje55555@gmail.com>
2025-01-13 18:13:13 +0530

    db connection and Task basic functionality added

---
+++ src/main/java/com/app/todoapp/services/TaskService.java
@@ -0,0 +1,21 @@
+package com.app.todoapp.services;
+
+import com.app.todoapp.models.Task;
+import org.springframework.stereotype.Service;
+import com.app.todoapp.repository.TaskRepository;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll() ;
+    }
+}
